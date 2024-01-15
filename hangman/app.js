05774dad0ea7303ec6c6d3e37c0a13d43e297168
ValeriaStav <valeriiastavriadi@gmail.com>
2024-01-15 17:07:21 +0300

    feat: add elements to quiz part

--- hangman/app.js
+++ hangman/app.js
@@ -1,65 +1,78 @@
 /* HEADER */
 
-const header = document.createElement("header")
-header.classList.add("header")
-document.body.append(header)
+const header = document.createElement("header");
+header.classList.add("header");
+document.body.append(header);
 
-const logo = document.createElement("img")
-logo.classList.add("header-img")
-logo.src = "./assets/images/hangman-game.png"
-header.appendChild(logo)
+const logo = document.createElement("img");
+logo.classList.add("header-img");
+logo.src = "./assets/images/hangman-game.png";
+header.appendChild(logo);
 
-const title = document.createElement("h1")
-title.classList.add("header-title")
-title.textContent = "Hangman Game"
-header.appendChild(title)
+const title = document.createElement("h1");
+title.classList.add("header-title");
+title.textContent = "Hangman Game";
+header.appendChild(title);
 
 /* PLAYING FIELD */
 
-const app = document.createElement("main")
-app.classList.add("app-container")
-document.body.append(app)
+const app = document.createElement("main");
+app.classList.add("app-container");
+document.body.append(app);
 
 /* Gallows Part */
 
-const gallowsPart = document.createElement("div")
-gallowsPart.classList.add("gallows-part")
-app.appendChild(gallowsPart)
+const hangmanBox = document.createElement("div");
+hangmanBox.classList.add("hangman-box");
+app.appendChild(hangmanBox);
 
-const gallowsImg = document.createElement("img")
-gallowsImg.classList.add("gallows-img")
-gallowsImg.src = "./assets/images/gallows.svg"
-gallowsPart.appendChild(gallowsImg)
+const hangmanImg = document.createElement("img");
+hangmanImg.classList.add("hangman-img");
+hangmanBox.appendChild(hangmanImg);
 
 /* Quiz Part */
 
-const quizPart = document.createElement("div")
-quizPart.classList.add("quiz-part")
-app.appendChild(quizPart)
+const quizPart = document.createElement("div");
+quizPart.classList.add("quiz-part");
+app.appendChild(quizPart);
 
-const secretWord = document.createElement("div")
-secretWord.classList.add("secret-word")
-secretWord.textContent = "_ _ _ _ _ _ _"
-quizPart.appendChild(secretWord)
+const secretWord = document.createElement("ul");
+secretWord.classList.add("secret-word");
+quizPart.appendChild(secretWord);
 
-const question = document.createElement("div")
-question.classList.add("question")
-question.textContent =
-  "Hint: any question  any question any question any question"
-quizPart.appendChild(question)
+const hintBox = document.createElement("div");
+hintBox.classList.add("hint-box");
+hintBox.textContent = "Hint: ";
+quizPart.appendChild(hintBox);
 
-const guessesCounter = document.createElement("div")
-guessesCounter.classList.add("guesses-counter")
-guessesCounter.textContent = "Incorrect guesses: 0/6"
-quizPart.appendChild(guessesCounter)
+const hint = document.createElement("span");
+hint.classList.add("hint");
+hintBox.appendChild(hint);
 
-const virtualKeyboard = document.createElement("div")
-virtualKeyboard.classList.add("keyboard")
+const guessesBox = document.createElement("div");
+guessesBox.classList.add("guesses-box");
+quizPart.appendChild(guessesBox);
+
+const guessesText = document.createElement("span");
+guessesText.classList.add("guesses-text");
+guessesText.textContent = "Incorrect guesses: ";
+guessesBox.appendChild(guessesText);
+
+const guessesCounter = document.createElement("span");
+guessesCounter.classList.add("guesses-counter");
+guessesBox.appendChild(guessesCounter);
+
+const virtualKeyboard = document.createElement("div");
+virtualKeyboard.classList.add("keyboard");
 
 for (let i = 65; i <= 90; i++) {
-  const keyBtn = document.createElement("button")
-  keyBtn.innerText = String.fromCharCode(i)
-  virtualKeyboard.appendChild(keyBtn)
+  const button = document.createElement("button");
+  button.classList.add("key-btn");
+  button.textContent = String.fromCharCode(i);
+  virtualKeyboard.appendChild(button);
+  button.addEventListener("click", (e) =>
+    initGame(e.target, String.fromCharCode(i))
+  );
 }
 
-quizPart.appendChild(virtualKeyboard)
+quizPart.appendChild(virtualKeyboard);
